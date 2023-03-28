b2bce21037f3cd0db0967fb4f39334cac52417eb
Raziloo <waissraz27@gmail.com>
2023-03-28 20:26:58 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+const app = express();
+
+mailchimp.setConfig({
+    apiKey: "c8eed2c585f6742ed3b194d62b6b555f",
+    server: "us11"
+  });
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    const listId = "ab7e448ca6";
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    async function run() {
+
+          const response = await mailchimp.lists.addListMember(listId, {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: firstName,
+              LNAME: lastName
+            }
+          });
+
+      }
+
+      function successCallback(result) {
+        res.sendFile(__dirname + '/success.html')
+ 
+    }
+    function failureCallback(error) {
+        res.sendFile(__dirname + '/failure.html')
+ 
+    }
+
+    run().then(successCallback, failureCallback);
+
+});
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.");
+});
+
+// api - c8eed2c585f6742ed3b194d62b6b555f-us11
+// audience id - ab7e448ca6
+
